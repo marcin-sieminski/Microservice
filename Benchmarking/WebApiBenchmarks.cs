fefde5f7571cf3e297b62e5a239bf5d32eea4a38
Marcin Siemiński <marcin.sieminski@outlook.com>
2024-07-06 11:13:25 +0200

    Add benchmark

---
+++ Benchmarking/WebApiBenchmarks.cs
@@ -0,0 +1,21 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Benchmarking;
+
+[MemoryDiagnoser]
+[ThreadingDiagnoser]
+public class WebApiBenchmarks
+{
+    private static HttpClient httpClient = new()
+    {
+        BaseAddress = new Uri("http://localhost:5198"),
+    };
+
+    [Benchmark]
+    public async Task GetBenchmark()
+    {
+        using HttpResponseMessage response = await httpClient.GetAsync("item");
+        response.EnsureSuccessStatusCode();
+        var jsonResponse = await response.Content.ReadAsStringAsync();
+    }
+}
